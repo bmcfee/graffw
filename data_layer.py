5b780ffb9868e3eb1df88d5383739ba6e5bbd531
Brian McFee <brian.mcfee@nyu.edu>
2014-10-26 03:11:45 -0400

    initial push

---
+++ data_layer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cPickle as pickle
+import ujson as json
+import os
+import glob
+
+#-- collection functions
+#-- track functions
+def get_track_audio(track_id):
+    
+    track = json.load(open('data/%08d.json' % track_id))
+
+    return track['filename']
+
+
+def get_track_analysis(track_id):
+
+    analysis = json.load(open('data/%08d.json' % track_id))
+    analysis['track_id'] = track_id
+
+    return analysis
